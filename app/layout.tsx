e14c0f87cc99b72491b0d75c07ee5e4377dbee51
Gagan <gaganbindoria2002@gmail.com>
2023-05-22 12:08:27 +0530

    deployment fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { QueryClientProvider, QueryClient } from "react-query";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import { ReactQueryDevtools } from "react-query/devtools";
@@ -17,17 +16,14 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const queryClient = new QueryClient();
   return (
     <html>
       <body className={inter.className}>
-        <QueryClientProvider client={queryClient}>
           <div className="bg-[#F3F3F3]">
             <NavBar></NavBar>
             <div>{children}</div>
             <ReactQueryDevtools initialIsOpen={false} />
           </div>
-        </QueryClientProvider>
       </body>
     </html>
   );
