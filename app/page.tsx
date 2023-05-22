033bc1111297999f8032dc8795abf7bc0b87ce63
Gagan <gaganbindoria2002@gmail.com>
2023-05-22 14:30:02 +0530

    updated navbar

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import NavBar from "../src/components/navbar";
 
 export default function Home() {
   return (
-    <div className="text-gray-800 h-[calc(100vh-100px)]">
+    <div className="text-gray-800 h-[calc(100vh-100px)] m-4">
       <div>This is home page </div>
       <div>
         Please click on <b>Dashboard</b> on navbar{" "}
