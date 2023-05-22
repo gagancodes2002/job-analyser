b99b8619621dee3231326908d8c32c3b5ad4e42f
Gagan <gaganbindoria2002@gmail.com>
2023-05-22 14:27:09 +0530

    fixed small bug on Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,9 @@ export default function Home() {
   return (
     <div className="text-gray-800 h-[calc(100vh-100px)]">
       <div>This is home page </div>
-      <div>Please click on "Dashboard" on navbar </div>
+      <div>
+        Please click on <b>Dashboard</b> on navbar{" "}
+      </div>
     </div>
   );
 }
